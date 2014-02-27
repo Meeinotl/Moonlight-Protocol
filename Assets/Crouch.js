046f62af34f483fef17c6125308aad04c7fa5321
Meeinotl <meeinotl@gmail.com>
2014-02-26 20:50:26 -0700

    Shootin' n' Sneakin'
    Basic behaviourial scripts written for crouching and shooting.
    + Left Shift to crouch
    + Crouching reduces the y scale of the first person controller
    
    + Left Mouse Button to shoot
    + Bullets disappear after a short time
    
    Known Issues:
    - Crouching currently reduces the y scale of the gun as well

---
+++ Assets/Crouch.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+public var CrouchAmount = 0.5;
+
+function Start () 
+{
+
+}
+
+function Update () 
+{
+	if (Input.GetButtonDown("Crouch"))
+	{
+		this.transform.localScale += Vector3(0, -0.5, 0);
+		this.transform.position.y -= CrouchAmount;
+	}
+	else if (Input.GetButtonUp("Crouch"))
+	{
+		this.transform.localScale += Vector3(0, 0.5, 0);
+		this.transform.position.y += CrouchAmount;
+	}
+}
---
+++ Assets/ShootBullet.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+var bullet: GameObject;
+//var bullet : Rigidbody;
+
+function Start () {
+
+}
+
+function Update () 
+{
+	if (Input.GetButtonDown("Fire1"))
+	{
+		var clone : GameObject;
+		clone = Instantiate(bullet, this.transform.position, this.transform.rotation);
+		//clone.rigidbody.velocity = transform.TransformDirection(Vector3.forward * 10);
+		clone.rigidbody.AddRelativeForce(-1000, 0, 0);
+		Destroy(clone, 3);
+	}
+}
