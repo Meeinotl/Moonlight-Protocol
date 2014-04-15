4c155b6415c4bda07d5e4ff1c646fa3d650e6158
Meeinotl <meeinotl@gmail.com>
2014-04-14 20:16:47 -0600

    Moving Platforms

---
+++ Assets/Elevator.js
@@ -0,0 +1,29 @@
+﻿#pragma strict
+
+var up = true;
+
+function Start () 
+{
+	
+}
+
+function Update () 
+{
+	if(transform.position.y > 5)
+	{
+		up = false;
+	}
+	else if(transform.position.y < 0)
+	{
+		up = true;
+	}
+	
+	if (up)
+	{
+		transform.position.y += .005;
+	}
+	else
+	{
+		transform.position.y -= .005;
+	}
+}
