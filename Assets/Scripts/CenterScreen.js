6c8c428f319a272efb2de9f35220a0d2943b249c
Meeinotl <meeinotl@yahoo.com>
2014-05-09 14:35:31 -0600

    Actually adding stuff;

---
+++ Assets/Scripts/CenterScreen.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+public var width = 1024;
+public var height = 512;
+public var cam : Camera;
+
+function Start () 
+{
+	transform.position = Vector3.zero;
+	transform.localScale = Vector3.zero;
+	guiTexture.pixelInset = Rect(cam.pixelWidth / 2 - width / 2, cam.pixelHeight / 2 - height / 2, width, height);
+}
+
+function Update () 
+{
+
+}
---
+++ Assets/Tutorial/PopUp.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+function Start () 
+{
+	transform.position = Vector3.zero;
+	transform.localScale = Vector3.zero;
+	guiTexture.pixelInset = Rect(0, 0, 301, 150);
+}
+
+function Update () 
+{
+
+}
